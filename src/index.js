5b041ceee0d53112e16452a6e8598ea7e253986a
Kim-Sarang-S2 <sarang4264@naver.com>
2023-11-25 16:35:00 +0900

    VocaPage 생성

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import VocaModal from "./jsx/VocaModal/VocaModal";
+// import VocaModal from "./jsx/VocaModal/VocaModal";
+import VocaPage from "./jsx/VocaPage/VocaPage";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   // <App />
+  // <VocaModal/>
   <>
-    <VocaModal/>
+    <VocaPage/>
   </>
-  // {/* </React.StrictMode> */}
+  // </React.StrictMode>
 );
 
 reportWebVitals();
