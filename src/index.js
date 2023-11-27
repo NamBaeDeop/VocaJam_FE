1c0cf48f674f5a9a7763aed8a77c4740a268eaa6
Kim-Sarang-S2 <sarang4264@naver.com>
2023-11-27 19:02:51 +0900

    브랜치 옮기겟음

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 // import VocaModal from "./jsx/VocaModal/VocaModal";
-import VocaPage from "./jsx/VocaPage/VocaPage";
+// import VocaPage from "./jsx/VocaPage/VocaPage";
 import reportWebVitals from "./reportWebVitals";
+import VocaCard from "./jsx/VocaCard/VocaCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   // <App />
-  // <VocaModal/>
+  // <VocaPage/>
   <>
-    <VocaPage/>
+    <VocaCard/>
   </>
   // </React.StrictMode>
 );
--- src/jsx/VocaPage/VocaPage.jsx
+++ src/jsx/VocaPage/VocaPage.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 // import { useState, useEffect } from "react";
 // import '../../css/vocaModal.css';
-
+import VocaCard from "./jsx/VocaCard/VocaCard";
 
 export default function VocaPage(){
 
     return (
         <>
             <section>
-                <div className="con_wrap">
-                    <div className="con">
-                        <h1>일본어</h1>
-                    </div>
-                </div>
+                <h1>일본어</h1>
+                <VocaCard />
             </section>
         </>
     )
