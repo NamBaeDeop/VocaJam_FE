4bfafcf5e97ff22cc31208cd2faa1a27786dd781
Kim-Sarang-S2 <sarang4264@naver.com>
2023-11-30 14:21:30 +0900

    styled-component 적용 : vocaPage.css 삭제, Vocapage.js 추가

---
+++ src/css/Vocapage.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+    margin-top: 26px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > h1{
+        width: 270px;
+    }
+
+`
+
+export default Section;
--- src/jsx/VocaPage/VocaPage.jsx
+++ src/jsx/VocaPage/VocaPage.jsx
@@ -2,17 +2,18 @@ import React from "react";
 // import { useState, useEffect } from "react";
 // import '../../css/vocaModal.css';
 import VocaCard from "../VocaCard/VocaCard";
-import '../../css/vocaPage.css';
+// import '../../css/vocaPage.css';
+import Section from "../../css/Vocapage";
 
 export default function VocaPage(){
 
     return (
         <>
-            <section>
+            <Section>
                 <h1>일본어</h1>
                 <VocaCard />
                 {/* map으로 반복 */}
-            </section>
+            </Section>
         </>
     )
 }
