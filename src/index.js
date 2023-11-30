a9090e8fd8575353b8618cb0de97c3f098e4a384
hyeisfree <nahye6559@naver.com>
2023-11-30 21:31:31 +0900

    .

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import VocaMain from "./jsx/VocaMain/VocaMain";
-// import VocaAdd from "../src/jsx/VocaMain/VocaAdd";
-import VocaMain from "./jsx/VocaMain/VocaMain";
+import App from "./App";
 // import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <VocaMain />
+  <App />
   // {/* </React.StrictMode> */}
 );
 
