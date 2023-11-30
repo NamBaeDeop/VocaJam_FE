e994370a4c02b270f1fdb376148a5fe14f286a3b
Kim-Sarang-S2 <sarang4264@naver.com>
2023-11-30 17:26:58 +0900

    css -> styled-component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Header from './jsx/default/Header'
 import Footer from './jsx/default/Footer';
 import VocaPage from './jsx/VocaPage/VocaPage';
 import './css/wrap.css';
+import VocaModal from './jsx/VocaModal/VocaModal';
 
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <VocaPage/>
-      <Footer/>
+      <Footer/> */}
+      <VocaModal/>
     </>
   );
 }
---
+++ src/css/VocaModal.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Modal = styled.div`
+    width: 250px;
+    height: 245px;
+    background-color: #EFAAAA;
+    border-radius: 20px;
+    border: 4px solid #C77D7D;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 250px;
+    left: 182px;
+`
+export const TitleExit= styled.div`
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    `
+export const TopCon = styled.div`
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &.word{
+    font-size: 27px;
+    font-weight: bold;
+    }
+    
+    &.exit{
+    font-size: 20px;
+    margin-left: 20px;
+    cursor: pointer;
+    }
+    `
+export const Btns = styled.div`
+    width: 100%;
+    height: 140px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    `
+
+export const Btn = styled.div`
+    width: 170px;
+    height: 55px;
+    background-color: white;
+    border-radius: 10px;
+    cursor: pointer;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 20px;
+    
+    `
