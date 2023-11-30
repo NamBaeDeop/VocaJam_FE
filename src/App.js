0e09aed313855dfde599888b0ee97c07a191b532
Sarang Kim <129057008+Kim-Sarang-S2@users.noreply.github.com>
2023-11-30 20:42:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import VocaPage from './jsx/VocaPage/VocaPage';
-import Game from "./jsx/game/Game";
+// import Game from "./jsx/game/Game";
 import Header from "./jsx/default/Header";
 import Footer from "./jsx/default/Footer";
 
