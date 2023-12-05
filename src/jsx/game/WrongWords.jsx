82221962466ed5198660e55c58a79c4632a9b787
seoyerin <seyerinq@gmail.com>
2023-12-05 22:05:51 +0900

    오류 수정

--- src/jsx/game/WrongWords.jsx
+++ src/jsx/game/WrongWords.jsx
@@ -16,10 +16,9 @@ function WrongWords() {
       <Wrong className={wrongWordList ? "" : "remove"}>
         <h2>틀린 단어</h2>
         <WordCardStyle className="wordList">
-          {wrongWordArr.current.map(
-            // (x) => console.log()
+          {wrongWordArr.current.map((x) => (
             <WordCard x={x} lan={lan} />
-          )}
+          ))}
         </WordCardStyle>
         <div
           className="retry"
